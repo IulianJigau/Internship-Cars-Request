098df75b931f75fa71d9e93d03cc64d51e5de600
Delirian <jigau.iulian@gmail.com>
2025-08-06 11:43:27 +0300

    Fixed a major error

--- src/main/java/com/scrapperapp/app/Defaults.java
+++ src/main/java/com/scrapperapp/app/Defaults.java
@@ -4,15 +4,18 @@ public class Defaults {
 
     public final int PAGE_SIZE;
     public final String SERVER_BASE_URL;
+    public final String BOT_TOKEN;
     
     public final String BOT_PATH = "/webhook";
     public final String TARGET_BASE_URL = "https://999.md/";
     public final String TARGET_API_PATH = "graphql";
 
-    public final String BOT_TOKEN = "8444077982:AAGr40Dk7vnqPYDmpo2YfZgjjpo_Z7l9Ee8";
     public final String BOT_USERNAME = "CScrapperBot";
 
     public Defaults() {
+        String botTokenEnv = System.getenv("BOT_TOKEN");
+        BOT_TOKEN = botTokenEnv != null ? botTokenEnv : "none";
+
         String pageSizeEnv = System.getenv("PAGE_SIZE");
         PAGE_SIZE = pageSizeEnv != null ? Integer.parseInt(pageSizeEnv) : 100;
 
