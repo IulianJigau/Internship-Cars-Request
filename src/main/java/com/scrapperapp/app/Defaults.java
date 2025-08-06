43734ac04da028dbabb522d34c9466ec974a3b13
Delirian <jigau.iulian@gmail.com>
2025-08-06 13:15:50 +0300

    Small fix

--- src/main/java/com/scrapperapp/app/Defaults.java
+++ src/main/java/com/scrapperapp/app/Defaults.java
@@ -3,8 +3,8 @@ package com.scrapperapp.app;
 public class Defaults {
 
     public final int PAGE_SIZE;
-    public final String SERVER_BASE_URL;
     public final String BOT_TOKEN;
+    public final String SERVER_BASE_URL;
     
     public final String BOT_PATH = "/webhook";
     public final String TARGET_BASE_URL = "https://999.md/";
@@ -16,10 +16,11 @@ public class Defaults {
         String botTokenEnv = System.getenv("BOT_TOKEN");
         BOT_TOKEN = botTokenEnv != null ? botTokenEnv : "none";
 
+        String serverUrlEnv = System.getenv("SERVER_BASE_URL");
+        SERVER_BASE_URL = serverUrlEnv != null ? serverUrlEnv : "localhost";
+
         String pageSizeEnv = System.getenv("PAGE_SIZE");
         PAGE_SIZE = pageSizeEnv != null ? Integer.parseInt(pageSizeEnv) : 100;
 
-        String serverUrlEnv = System.getenv("SERVER_BASE_URL");
-        SERVER_BASE_URL = serverUrlEnv != null ? serverUrlEnv : "localhost";
     }
 }
